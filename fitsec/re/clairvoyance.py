50d4ebf06e14c21a61e3c41e0b2aefdee84d8e61
Tyler Dionne <143131384+tylerdionne@users.noreply.github.com>
2024-02-18 17:47:31 -0500

    Delete fitsec directory

--- fitsec/re/clairvoyance.py
+++
@@ -1,7 +0,0 @@
-from pwn import *
-#p=remote("fitsec-clairvoyance.chals.io", 443, ssl=True, sni="fitsec-clairvoyance.chals.io")
-p = process("./clairvoyance.bin")
-p.recvuntil(">>>")
-p.sendline()
-p.interactive()
-
