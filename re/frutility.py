78237953657294877fdf811d919a9109b75a918f
Tyler Dionne <143131384+tylerdionne@users.noreply.github.com>
2024-02-19 08:47:49 -0500

    Delete re directory

--- re/frutility.py
+++
@@ -1,6 +0,0 @@
-from pwn import *
-#p=remote("fitsec-futility.chals.io", 443, ssl=True, sni="fitsec-futility.chals.io")
-p = process("./frutility.bin")
-p.recvuntil(">>>")
-p.sendline(b'notepad')
-p.interactive()
